e50231e0da1f9db4496853082d7ed7d0445c5300
AngelovD <71969791+AngelovD@users.noreply.github.com>
2021-02-17 09:22:38 +0200

    Add files via upload

--- 20210215/20210215_12.c
+++ 20210215/20210215_12.c
@@ -33,7 +33,17 @@ float getAverage(){
         total+=matrix[i][j][z%10];
         z++;
     }
-    printf("Total %d\n",total);
+    printf("Total1 %d\n",total);
+    i=0;j=0;z=0;
+    int *p=&matrix[0][0][0];
+    total=0;
+    while(z<1000){
+        i=z/100;
+        j=(z%100)/10;
+        total+=*(p+(i*100)+(j*10)+(z%10));
+        z++;
+    }
+    printf("Total2 %d\n",total);
 
     rez=(float)total/1000;
 }
